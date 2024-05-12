67b4ddd0eb5344bcb2b87bc07cf9432314222546
Ashim Rudra Paul <codewithashim@gmail.com>
2024-05-12 15:27:32 +0600

    changes

--- src/app/AppWrappers.tsx
+++ src/app/AppWrappers.tsx
@@ -1,3 +1,26 @@
+// 'use client';
+// import React, { ReactNode } from 'react';
+// import 'styles/App.css';
+// import 'styles/Contact.css';
+// // import '@asseinfo/react-kanban/dist/styles.css';
+// // import 'styles/Plugins.css';
+// import 'styles/MiniCalendar.css';
+// import 'styles/index.css';
+
+// import dynamic from 'next/dynamic';
+
+// const _NoSSR = ({ children }) => <React.Fragment>{children}</React.Fragment>;
+
+// const NoSSR = dynamic(() => Promise.resolve(_NoSSR), {
+//   ssr: false,
+// });
+
+// export default function AppWrappers({ children }: { children: ReactNode }) {
+//   // @ts-expect-error
+//   return <NoSSR>{children}</NoSSR>;
+// }
+
+
 'use client';
 import React, { ReactNode } from 'react';
 import 'styles/App.css';
@@ -9,13 +32,15 @@ import 'styles/index.css';
 
 import dynamic from 'next/dynamic';
 
-const _NoSSR = ({ children }) => <React.Fragment>{children}</React.Fragment>;
+// Define a placeholder component for NoSSR
+const _NoSSR = ({ children }: any) => <React.Fragment>{children}</React.Fragment>;
 
+// Dynamically import the NoSSR component with ssr option set to false
 const NoSSR = dynamic(() => Promise.resolve(_NoSSR), {
   ssr: false,
 });
 
+// Define AppWrappers component to wrap children with NoSSR
 export default function AppWrappers({ children }: { children: ReactNode }) {
-  // @ts-expect-error
   return <NoSSR>{children}</NoSSR>;
 }
